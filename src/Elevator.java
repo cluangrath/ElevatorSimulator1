8c4bc7f7a8528fbeb4bae764e77cb468ea00fc98
cluangrath <cluangrath1@gmail.com>
2014-10-29 22:25:32 -0500

    more elevator updates
    updates to the elevator class. findNextDestinationFloor method is more
    efficient and searches rider arraylist to remove the next destination
    floor.
    
    Ryan - search “RYAN” inside of the elevator class to see what comments
    i’ve left for you

--- src/Elevator.java
+++ src/Elevator.java
@@ -5,6 +5,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Elevator implements Runnable {
 
+	//RYAN: was this what you had in mind?
+	public static LinkedList<LinkedList<Person>> floorQueues = new LinkedList<LinkedList<Person>>(); //the index of the linkedlist is the floor in which it will represent
+	
+	public String elevatorName;
 	public int currentFloor;
 	public int destinationFloor;
 	public int direction; //1 for up, -1 for down
@@ -14,27 +18,31 @@ public class Elevator implements Runnable {
 	private Lock elevatorLock;
 	private Condition elevatorCondition;
 
-	public Elevator() {
+	public Elevator(String name) {
+		this.elevatorName = name;
 		this.riders = new ArrayList<Person>();
-		this.currentFloor = 15; //start all elevators created at floor 0
+		this.currentFloor = 0; //start all elevators created at floor 0
 		this.elevatorLock = new ReentrantLock();
 		this.elevatorCondition = this.elevatorLock.newCondition();
-		this.direction = -1;
+		this.direction = 1;
 	}
-
-	@Override
+	
 	public void run() {
-		System.out.println("Locking the resource");
-		this.elevatorLock.lock();
-		try
-		{
-			System.out.println("In the run method from elevator class");
-			System.out.println("next destination floor for elevator 1: " + findNextDestinationFloor());
-		}
-		finally
-		{
-			System.out.println("Releasing the lock");
-			this.elevatorLock.unlock();
+		int nextFloor = findNextDestinationFloor();
+		while (nextFloor != -1) {
+			//RYAN: continue this elevator until the riders are completely gone... 
+				//probably not a good idea because there might be a period when the elevator has no riders at a given moment, meaning the elevator will then shut down and not start up again
+			System.out.println(this.elevatorName + " serviced floor " + nextFloor);
+			this.currentFloor = nextFloor;
+			nextFloor = findNextDestinationFloor();
+			
+			try {
+				Thread.sleep(1000); //the elevator sleeps for 1 second at every floor to allow people to enter/exit...
+				//RYAN: how does the elevator know who it needs to pick up? - look at the queue for this floor
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -55,10 +63,12 @@ public class Elevator implements Runnable {
 				if (floor > this.currentFloor)
 				{
 					upperFloors.add(floor);
+					//System.out.println("Added floor " + floor);
 				}
 				else
 				{
 					lowerFloors.add(floor);
+					//System.out.println("Added floor " + floor);
 				}
 			}
 		}
@@ -66,24 +76,40 @@ public class Elevator implements Runnable {
 		if (this.direction == 1)
 		{
 			//the elevator is going up
-			closestFloor = upperFloors.peekFirst();
-			for(int i : upperFloors)
-			{
-				if (i < closestFloor)
+			if (upperFloors.peekFirst() != null) {
+				closestFloor = upperFloors.peekFirst();
+				for(int i : upperFloors)
 				{
-					closestFloor = i;
-				}
+					if (i < closestFloor)
+					{
+						closestFloor = i;
+					}
+				}	
 			}
 		}
 		else
 		{
 			//the elevator is going down
-			closestFloor = lowerFloors.peekFirst();
-			for(int i : lowerFloors)
-			{
-				if (i > closestFloor)
+			if (lowerFloors.peekFirst() != null) {
+				closestFloor = lowerFloors.peekFirst();
+				for(int i : lowerFloors)
 				{
-					closestFloor = i;
+					if (i > closestFloor)
+					{
+						closestFloor = i;
+					}
+				}
+			}
+		}
+				
+		//RYAN: this loop will cycle through all of the riders in the elevator and remove the 'next' floor from all of the people that are going to that floor.
+			//in second thoughs - this isn't going to work because it's going to remove the floor period even if person A is going there now, and person B is going there as their last floor
+		for(Person p : this.riders) {
+			for(int i = 0; i < p.desiredFloors.size(); i++) {
+				int nextLocation = p.desiredFloors.indexOf(closestFloor);
+				if (nextLocation > -1) {
+					//System.out.println("Removed floor " + p.desiredFloors.remove(nextLocation));
+					p.desiredFloors.remove(nextLocation);
 				}
 			}
 		}
--- src/Person.java
+++ src/Person.java
@@ -4,23 +4,30 @@ import java.util.*;
 public class Person implements Runnable {
 	
 	public String name;
-	public ArrayList<Integer> desiredFloors;
+	public LinkedList<Integer> desiredFloors;
 	public int startingFloor;
-	public int floorIdleTime;
+	public long floorIdleTime;
 	
-	public Person(String name, ArrayList<Integer> desiredFloors, int startFloor, int floorIdleTime)
+	public Person(String name, LinkedList<Integer> desiredFloors, int startFloor, int floorIdleTime)
 	{
 		this.name = name;
 		this.desiredFloors = desiredFloors;
 		this.startingFloor = startFloor;
-		this.floorIdleTime = floorIdleTime;
+		this.floorIdleTime = new Long(floorIdleTime * 1000); // create the floorIdleTime as a long int in milliseconds
 	}
 
-	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("in the person run method!");
-		
-		System.out.println(this.name + " is starting on floor " + this.startingFloor);
+//		System.out.println("in the person run method!");
+//		
+//		System.out.println(this.name + " is starting on floor " + this.startingFloor);
+//		
+//		try {
+//			//Thread.sleep(this.floorIdleTime); //start the person off by sleeping or "spending time on the floor"
+//			//System.out.println("Person thread slept for: " + this.floorIdleTime + " milliseconds");
+//			
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
